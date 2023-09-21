fc8e801c12fbccfb8395d308a1fa805dc00cbd63
TalGav23 <talgav23@gmail.com>
2023-09-21 21:35:41 +0300

    flip animation, need to fix

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
 import "./Card.css";
+import { useSpring, animated } from "@react-spring/web";
 
 const Card = (props) => {
   const { image, onCardClick } = props;
   const cardBack = "./space/back-card.png";
-  const [isFlipped, setIsFlipped] = useState(false); // Local state for each card
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const { transform } = useSpring({
+    transform: `perspective(600px) rotateY(${isFlipped ? 180 : 0}deg)`,
+  });
+
+  const { opacity } = useSpring({
+    opacity: isFlipped ? 0 : 1,
+    config: { duration: 250 }, // Adjust the duration (in milliseconds) as needed
+  });
 
   const handleCardClick = () => {
     setIsFlipped(!isFlipped);
@@ -12,15 +22,24 @@ const Card = (props) => {
   };
 
   return (
-    <div
-      className={`square shiny card-flip ${isFlipped ? "flipped" : ""}`}
-      onClick={handleCardClick}
-    >
-      <img
-        src={isFlipped ? `./space/${image}.png` : cardBack}
-        alt="hi"
-        className="card-picture center"
-      />
+    <div onClick={handleCardClick}>
+      <animated.div
+        className={`square shiny`}
+        style={{
+          transform,
+        }}
+      >
+        <div className={`card ${isFlipped ? "flipped" : ""}`}>
+          <animated.img
+            src={isFlipped ? `./space/${image}.png` : cardBack}
+            alt="hi"
+            className="card-picture center"
+            style={{
+              opacity,
+            }}
+          />
+        </div>
+      </animated.div>
     </div>
   );
 };
